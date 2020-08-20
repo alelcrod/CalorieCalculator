1d052a7e69f7260c58e85386fd16fe3ec9f5e9a3
Alex Rodriguez <alr18@terpmail.umd.edu>
2020-08-20 19:12:13 -0400

    project created

---
+++ CalorieCalculator/CalorieCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalorieCalculator
+//
+//  Created by Alex Rodriguez on 8/20/20.
+//  Copyright © 2020 Alex Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
